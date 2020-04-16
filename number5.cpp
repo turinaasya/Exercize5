c28ca95c522c64f19ad8612a244ac502aaa46e5e
turinaasya <63805882+turinaasya@users.noreply.github.com>
2020-04-16 20:24:49 +0300

    Add files via upload

---
+++ number5.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus"); 
+    double x; 
+    cout << "Введите x: ";
+    cin >> x;
+
+    double yn = x, 
+        yn1 = 0; 
+
+    while (true)
+    {
+        yn1 = 0.5 * (yn + 3 * x / (2 * yn * yn + x / yn));
+
+        if (abs(yn1 - yn) < pow(10.0, -5.0)) 
+            break; 
+
+        yn = yn1; 
+    }
+
+    cout << yn << endl;
+
+    system("pause");
+    return 0;
+}
